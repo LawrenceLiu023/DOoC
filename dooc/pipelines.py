5a740438f7761f8b4c41e491b84eec5af17c430e
liujiahuan <liujiahuan@ish.org.cn>
2024-06-03 09:54:26 +0800

    ft: pairwise inference

--- dooc/pipelines.py
+++ dooc/pipelines.py
@@ -2,6 +2,7 @@ import typing
 import torch
 from moltx import tokenizers
 from dooc import models
+from utils import pairwise_rank
 
 
 class MutSmi:
@@ -67,3 +68,34 @@ class MutSmiFullConnection(MutSmi):
         smi_src, smi_tgt, gene_src = self._model_args(gene, smiles)
         pred = self.model(smi_src, smi_tgt, gene_src)
         return pred.item()
+
+
+class MutSmiPairwiseLtr(MutSmi):
+    def __init__(
+        self,
+        smi_tokenizer: tokenizers.MoltxTokenizer,
+        model: models.MutSmiFullConnection,
+        device: torch.device = torch.device("cpu"),
+    ) -> None:
+        super().__init__(smi_tokenizer, model, device)
+
+    def __call__(
+        self, gene: typing.Sequence[int], smiles: typing.Sequence[str]
+    ) -> typing.Sequence[float]:
+        smi_src, gene_src = self._model_args(gene, smiles)
+
+        def pairwise_infer(a, b) -> int:
+            """
+            这是一个Pairwise推理的示例，模仿了Pairwise推理的函数形式，可根据两项给出代表大小关系的值。
+            """
+            VALID_PREFERENCE:dict[str, int] = {"gt": 1, "eq": 0, "lt": -1}
+            if a > b:
+                return VALID_PREFERENCE["gt"]
+            elif a == b:
+                return VALID_PREFERENCE["eq"]
+            else:
+                return VALID_PREFERENCE["lt"]
+
+        # 根据Pairwise推理，计算排序结果。三个参数分别为：需排序项的列表、Pairwise推理函数、比较结果为“小于”时推理函数的返回值
+        pred: typing.Sequence[int] = pairwise_rank(smi_src, pairwise_infer, -1)
+        return pred
--- dooc/utils.py
+++ dooc/utils.py
@@ -78,3 +78,77 @@ def load_ontology(file_name: str, gene2id_mapping: dict) -> typing.Sequence:
         )
 
     return dg, leaves[0], term_size_map, term_direct_gene_map
+
+
+class _ComparableItem:
+    """
+    将对象转化为可比较对象，从而对于列表，可以使用`sort()`进行排序。
+
+    Attributes
+    ----------
+    item : Any
+        需要添加比较功能的项，可以是任何类型。
+    compare_func : Callable
+        一个二元比较函数，大小比较功能基于这一函数实现。
+    lt_return : dict, default -1
+        当`compare_func`比较结果为小于时，返回的值。
+
+    Methods
+    -------
+    __lt__(other)
+        实现`<`比较运算符，python原生sort()依赖`<`运算符进行比较。如果不实现`<`运算符，`sort()`也可以支持`>`运算符。
+    """
+
+    def __init__(self, item, compare_func, lt_return=-1) -> None:
+        self.item = item
+        self.compare_func = compare_func
+        self.lt_return = lt_return
+
+    def __lt__(self, other) -> bool:
+        """实现`<`比较运算符，python原生sort()依赖`<`运算符进行比较"""
+        preference = self.compare_func(self.item, other.item)
+        if preference == self.lt_return:
+            return True
+        return False
+
+
+def pairwise_rank(
+    items: typing.Sequence,
+    compare_func: typing.Callable[[typing.Any, typing.Any], typing.Any],
+    lt_return: typing.Any = -1,
+) -> typing.Sequence[int]:
+    """
+    利用Pairwise推理的函数，得到全局排序结果。
+
+    Parameters
+    ----------
+    items : Sequence
+        需要比较的项。
+    compare_func : Callable
+        一个二元比较函数，返回一个表示大小比较结果的值。形式类似于`pair_compare(a, b) -> int`
+    lt_return : Any, default -1
+        `compare_func`比较结果为小于时，返回的值。
+
+    Returns
+    -------
+    rank_list : Sequence[int]
+        与输入的项`items`对应的排序编号列表。
+
+    Examples
+    --------
+    >>> items = ["1", "7", "2", "3", "0"]
+    >>> compare_func = lambda x, y: -1 if float(x) < float(y) else 1
+    >>> pairwise_rank(items, pairwise_infer)
+    [2, 5, 3, 4, 1]
+    """
+    comparable_items: list[_ComparableItem] = [
+        _ComparableItem(item, compare_func, lt_return) for item in items
+    ]
+    comparable_items_index_zip_list: list = list(
+        zip(comparable_items, list(range(len(comparable_items))))
+    )
+    comparable_items_index_zip_list.sort(key=lambda x: x[0])
+    rank_list: list = [0 for _ in range(len(comparable_items))]
+    for i, (_, original_index) in enumerate(comparable_items_index_zip_list):
+        rank_list[original_index] = i + 1
+    return rank_list
